b64c8ed2bbd78a5a4d849ecd53a09dfa93d45dd4
developerSeonah <salj3895.developer@gmail.com>
2024-11-19 09:45:46 +0900

    test02

--- src/main/java/test01/test01.java
+++ src/main/java/test01/test01.java
@@ -4,18 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+* 문제 풀이의 순서
+* 1) 문제를 먼저 읽어라.
+* 2) 문제를 먼저 풀어라.
+* 3) 문제를 푼 순서를 주석으로 (한글 코딩으로) 코드에 작성해라
+* 4) 주석 순서대로 코드 작성하기
+*       온라인 >> 주석제거
+*       오프라인 >> 주석 포함 !! **
+*
+*   api
+*   application programming interface
+*   코드 빌려쓰기?!
+*
+* */
+
 public class test01 {
     public static void main(String[] args) {
+
+        // 배열이 주어졌을 때
+        // 가장 큰 점수를 찾고,
+        // 가장 작은 점수를 찾는다.
+        // 큰 수 -  작은 점수 = 정답
+
         Scanner sc = new Scanner(System.in);
 
         List<Integer> score = new ArrayList<>();
 
         int n = sc.nextInt();
+        //int []  datas = new int[N];
+        /*
+        * for(int i = 0; i < datas.length; i++){
+        *   datas[i] = sc.nextInt();
+        * }
+        *
+        *  int max = Arrays.stream(datas).max().getAsInt();
+        *  int min = Arrays.stream(datas).min().getAsInt();
+        *
+        *   int res= max - min;
+        *   System.out.println(res);
+        * */
+
 
         for (int i = 0; i <n; i++){
             score.add(sc.nextInt());
         }
-
         int max = score.get(0);
 
         for (int i = 0; i <n; i++){
--- src/main/java/test02/test02.java
+++ src/main/java/test02/test02.java
@@ -1,7 +1,42 @@
 package test02;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class test02 {
+    /*
+     * "스택" 문제
+     * '직전의 숫자는 무시한다' - 실행 취소,  뒤로가기, Ctrl+z
+     *  = 선입 후출, FILO <<==
+     *
+     * */
+
     public static void main(String[] args) {
 
+        // N 크기 만큼의 스택 공간 형성
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<Integer>();
+
+        // ** ctrl c,v ==> 반복문
+        // ** While VS for
+        for(int i = 0; i < N; i++){
+            int data = sc.nextInt();
+            if(data != 0){
+                stack.push(data);
+            }else{
+                stack.pop();
+            }
+        }
+        // N 개의 데이터를 입력받을 것임
+        //      0이 아니면 그냥 스택에 저장저장
+        //      0이면 데이터 제거
+
+        // 마지막에 스택에 남아있는 모든 데이터 총합
+        int total = 0;
+        for(int v:stack){
+            total += v;
+        }
+        System.out.println(total);
     }
 }
